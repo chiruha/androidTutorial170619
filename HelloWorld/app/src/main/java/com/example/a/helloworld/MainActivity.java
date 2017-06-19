3d0ae551572bc321b54841304a8b57f534f68f5e
Your Name <you@example.com>
2017-06-19 15:54:49 +0900

    first commit

---
+++ HelloWorld/app/src/main/java/com/example/a/helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.a.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onHelloWorldClick(View view){
+        Toast.makeText(this, "Hello World Click", Toast.LENGTH_SHORT).show();
+    }
+
+}
