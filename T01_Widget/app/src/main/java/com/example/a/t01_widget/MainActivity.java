60f4c159025fc1068f2ef25fa976879f2fd3a2a6
Your Name <you@example.com>
2017-06-19 17:44:27 +0900

    6/19 수업

--- T01_Widget/app/src/main/java/com/example/a/t01_widget/MainActivity.java
+++ T01_Widget/app/src/main/java/com/example/a/t01_widget/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.a.t01_widget;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.widget);
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+
+        Button btnTest = (Button) findViewById(R.id.btnTest);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("WidgetTest","Button Click");
+                String str = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "str", Toast.LENGTH_SHORT).show();
+            }
+        });
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkbox);
+        checkBox.setChecked(true);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean b) {
+                Toast.makeText(MainActivity.this, "checked : "+b, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int i) {
+                switch (i){
+                    case R.id.radioButton1:
+                        Toast.makeText(MainActivity.this, "radio1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioButton2:
+                        Toast.makeText(MainActivity.this, "radio2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioButton3:
+                        Toast.makeText(MainActivity.this, "radio3", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+
+
     }
+
+
+
+
 }
